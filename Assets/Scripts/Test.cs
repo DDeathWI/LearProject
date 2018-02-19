a9fffbc5ec554beea9311d72fefa1e7a5e13d5e5
DDeathWI <vladislav.solovey1995@gmail.com>
2018-02-19 23:49:02 +0200

    Commit

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Test {
+
+    public static void ResetTrans(this Transform t) {
+
+        t.position = Vector3.zero;
+
+    }
+	
+	
+}
---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Testing : MonoBehaviour {
+
+    delegate void MyDelegate(int num);
+    MyDelegate myDelegate;
+
+
+    void Start()
+    {
+        myDelegate = PrintNum;
+
+
+        myDelegate += DoubleNum;
+
+        myDelegate(50);
+    }
+
+    void PrintNum(int num)
+    {
+        print("Print Num: " + num);
+    }
+
+    void DoubleNum(int num)
+    {
+        print("Double Num: " + num * 2);
+    }
+
+
+}
